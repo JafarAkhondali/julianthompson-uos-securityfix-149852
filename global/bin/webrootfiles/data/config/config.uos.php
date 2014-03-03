ebea76985ddac5abf489e517db2b501fc17b7e7e
julianthompson <julian@atomicant.co.uk>
2014-03-03 11:19:52 +0000

    Example config for new config structure defined in const.php

--- global/bin/webrootfiles/data/config/config.uos.php
+++ global/bin/webrootfiles/data/config/config.uos.php
@@ -10,6 +10,7 @@
 $uos->config->globaldbstring = UOS_DATABASE;
 $uos->config->debugmode = FALSE;
 $uos->config->showerrors = TRUE;
+$uos->config->logging = FALSE;
 
 
 // These items are only temporary dummy items to get us started
@@ -81,16 +82,18 @@ $uos->config->types = array(
 	),
 );
 
-$uos->config->data = array(
+$uos->config->data = new StdClass();
+
+$uos->config->data->entities = array(
 
 	4567898765 => (object) array(
 		'guid' => 4567898765,  
 		'type' => 'node_device_light', //attribute
 		'title' => 'Standard Lamp',
 		//'description' => "",
-		//'parents' => array('downstairs','lounge'),
-		'x10housecode' => 1,
-		'x10key' => 'a',
+		//'parents' => array(),
+		'x10housecode' => 'a',
+		'x10key' => 1,
 		'dimmable' => TRUE,
 		'level' => 100,
 		//'lastupdate' => 0
@@ -99,15 +102,13 @@ $uos->config->data = array(
 	8834323145 => (object) array(
 		'guid' => 8834323145,
 		'type' => 'node',
-		'title' => 'Greenacres',
-		'zones' => array('downstairs','lounge'),
+		'title' => 'Greenacres'
 	),
 
 	1313534234 => (object) array(
 		'guid' => 1313534234,
 		'type' => 'node',
 		'title' => "Atomic Ant",
-		'zones' => array('downstairs','lounge'),
 		'downloadurl' => "text/vcard:Atomic Ant.vcf:/data/1313534234/atomicant.vcf"
 	),
 	5645342341 => (object) array(
@@ -123,7 +124,8 @@ $uos->config->data = array(
 		'guid' => 3643827492,
 		'type' => 'node_person',
 		'title' => "Julian Thompson",
-//		'zones' => array('downstairs','lounge'),
+		'firstname' => 'Julian',
+		'lastname' => 'Thompson',
 //		'downloadurl' => "text/vcard:Julian Thompson.vcf:http://universeos.localhost/global/admin/dragdrop/data/1313534234/atomicant.vcf"
 	),
 
@@ -131,7 +133,8 @@ $uos->config->data = array(
 		'guid' => 6453127562,
 		'type' => 'node_person',
 		'title' => "Peter Barret",
-//		'zones' => array('downstairs','lounge'),
+		'firstname' => 'Peter',
+		'lastname' => 'Barret',
 //		'downloadurl' => "text/vcard:Peter Barret.vcf:http://universeos.localhost/global/admin/dragdrop/data/1313534234/atomicant.vcf"
 	),
 	
@@ -139,7 +142,8 @@ $uos->config->data = array(
 		'guid' => 5647646464,
 		'type' => 'node_person',
 		'title' => "Marek Sotak",
-//		'zones' => array('downstairs','lounge'),
+		'firstname' => 'Marek',
+		'lastname' => 'Sotak',
 //		'downloadurl' => "text/vcard:Marek Sotak.vcf:http://universeos.localhost/global/admin/dragdrop/data/1313534234/atomicant.vcf"
 	),
 	
@@ -147,7 +151,6 @@ $uos->config->data = array(
 		'guid' => 3344221134,
 		'type' => 'node_event',
 		'title' => "Julian Thompson Birthday",
-		'zones' => array('downstairs','lounge'),
 		'downloadurl' => "text/calendar:Julian Thompson Birthday.vcal:http://universeos.localhost/global/admin/dragdrop/data/3344221134/3344221134.vcal"
 	),
 	
@@ -156,7 +159,6 @@ $uos->config->data = array(
 		'type' => 'node_url',
 		'title' => "8 Cool Google Labs Projects Spared the Axe",
 		'url' => 'http://www.cio.com/article/692467/8_Cool_Google_Labs_Projects_Spared_the_Axe',
-		'zones' => array('downstairs','lounge'),
 		//'downloadurl' => "text/calendar:Julian Thompson Birthday.vcf:http://universeos.localhost/global/admin/dragdrop/data/3344221134/3344221134.ics"
 	),
 	5644564645 => (object) array(
@@ -164,67 +166,65 @@ $uos->config->data = array(
 		'type' => 'node_location',
 		'title' => "XX Court Farm Gardens",
 		//'url' => 'http://www.cio.com/article/692467/8_Cool_Google_Labs_Projects_Spared_the_Axe',
-		'zones' => array('downstairs','lounge'),
 		//'downloadurl' => "text/calendar:Julian Thompson Birthday.vcf:http://universeos.localhost/global/admin/dragdrop/data/3344221134/3344221134.ics"
 	),
 	2113564674 => (object) array(
 		'guid' => 2113564674,
 		'type' => 'node_message',
 		'title' => "Accounts to 31 december 2013",
-		'zones' => array('downstairs','lounge'),
 	),
 	5645625342 => (object) array(
 		'guid' => 5645625342,
 		'type' => 'node_file_video',
 		'title' => "Alan Watts",
-		'zones' => array('downstairs','lounge'),
 	),
 	3434652345 => (object) array(
 		'guid' => 3434652345,
 		'type' => 'node_device',
 		'title' => "Media Center",
-		'zones' => array('downstairs','lounge')	
+		'x10housecode' => 'a',
+		'x10key' => 2
 	),
 	7665563464 => (object) array(
 		'guid' => 7665563464,
 		'type' => 'node_file',
-		'title' => "The Voynich Manuscript complete",
-		'zones' => array('downstairs','lounge')	
+		'title' => "The Voynich Manuscript complete",	
 	),
 	2346828276 => (object) array(
 		'guid' => 2346828276,
 		'type' => 'node_note',
-		'title' => "The truth",
-		'zones' => array('downstairs','lounge')	
+		'title' => "The truth"	
 	),
 	5345428273 => (object) array(
 		'guid' => 5345428273,
 		'type' => 'node_service_google',
-		'title' => "mistergroove@atomicant.co.uk",
-		'zones' => array('downstairs','lounge')	
+		'title' => "mistergroove@atomicant.co.uk"	
 	),
 	2827623468 => (object) array(
 		'guid' => 2827623468,
 		'type' => 'node_service_google',
-		'title' => "xxx@atomicant.co.uk",
-		'zones' => array('downstairs','lounge')	
+		'title' => "xxx@atomicant.co.uk"
 	),
 	3246824231 => (object) array(
 		'guid' => 3246824231,
 		'type' => 'node_emailaddress',
-		'title' => "xxx@atomicant.co.uk",
-		'zones' => array('downstairs','lounge')	
+		'title' => "xxx@atomicant.co.uk"
 	),
 	4674646465 => (object) array(
 		'guid' => 4674646465,
 		'type' => 'node_phonenumber',
 		'title' => "+44 7958 903924",
 		'country' => 'uk',
-		'phonenumber' => '07958903924',
-		'zones' => array('downstairs','lounge')	
+		'phonenumber' => '07958903924'
 	)
 );
 
+$uos->config->data->children = array(
+	// Atomic Ant
+	1313534234 => array(3643827492,6453127562,5647646464,2345725347),
+	// Greenacres
+	8834323145 => array(4567898765,3434652345),
+);
 
 
 /* 
