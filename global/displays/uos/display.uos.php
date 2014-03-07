6774b06f0612d5a5eeda8ca8ec2b7e02a4e52074
Julian Thompson <julian@atomicant.co.uk>
2014-03-07 15:54:20 +0000

    Fixed html tag writing for PHP versions without anonymous functions

--- global/displays/uos/display.uos.php
+++ global/displays/uos/display.uos.php
@@ -1,12 +1,30 @@
-
 <?php
 
+
+// For PHP without Anonymous functions
+
+function DISPLAY_attributestostring($attributes) {
+	$keys = array_keys($attributes);
+	return join(' ', array_map(DISPLAY_attributestostring_callback,$keys,$attributes));
+}
+
+function DISPLAY_attributestostring_callback($key,$value) {
+   if (is_bool($value)) {
+      return $value?$value:'';
+   }
+   return $key.'="'.$value.'"';	
+}
+
+/*
 function DISPLAY_attributestostring($attributes) {
 	return join(' ', array_map(function($sKey) use ($attributes) {
-	   if(is_bool($attributes[$sKey]))
-	   {
+	
+	   if (is_bool($attributes[$sKey])) {
 	      return $attributes[$sKey]?$sKey:'';
 	   }
+
 	   return $sKey.'="'.$attributes[$sKey].'"';
+
 	}, array_keys($attributes)));
 }
+*/
