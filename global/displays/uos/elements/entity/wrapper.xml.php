7201d48d25d02676b4f4cbce025a233d931ac00a
Julian Thompson <julian@atomicant.co.uk>
2014-03-27 17:04:39 +0000

    Added new preprocess functions for node_file_images

--- global/displays/uos/elements/entity/wrapper.xml.php
+++ global/displays/uos/elements/entity/wrapper.xml.php
@@ -1,3 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<?php if (isset($uos->output['resources']['xsl'])) : ?>
+<?xml-stylesheet type="text/xsl" href="<?php print $uos->output['resources']['xsl'][0];?>"?>
+<?php endif; ?>
+
 <<?php print $render->wrapperelement;?>>
 <?php print $render->templateoutput;?>
 
