8167e49a254a5eb267f4c62740e1db6650f3f35c
Julian Thompson <julian@atomicant.co.uk>
2014-04-07 18:57:09 +0100

    Fixed file upload for non spaced filenames

--- global/displays/uos/elements/template.uosio.php
+++ global/displays/uos/elements/template.uosio.php
@@ -2,6 +2,7 @@
 //echo "xxx";
 $display = isset($uos->request->parameters['display'])?$uos->request->parameters['display']:'html';
 $content = rendernew($entity,array('displaystring'=>'html'));
+$content = rendernew($entity,array('displaystring'=>$display));
 $elementdata = isset($uos->output['elementdata'])?$uos->output['elementdata']:array();
 print json_encode( (object) array(
 	'content'=>$content,
